1afcb80c64a9e1146063736ca035c79145e025f9
czhu1231 <clairezhu05@gmail.com>
2025-01-30 19:53:58 -0500

    resultstesting

--- frontend/src/components/TopAlbums.js
+++ frontend/src/components/TopAlbums.js
@@ -1,50 +1,86 @@
 import React from 'react';
-import TrackCard from './TrackCard.js';
+import TrackCard from './TrackCard';
 import './TopAlbums.css';
 //use props!
 
 const one = (
     <TrackCard 
-        // albumName=
-        // artistName =
-        // img = 
-         />
+        albumName= 'Espresso'
+        artistName = 'Sabrina Carpenter'
+        img = 'https://i.scdn.co/image/ab67616d00001e02659cd4673230913b3918e0d5'
+    />
 )
 
 const two = (
-    <TrackCard/>
+    <TrackCard
+        albumName= 'Beautiful Things'
+        artistName = 'Benson Boone'
+        img = 'https://i.scdn.co/image/ab67616d00001e02bef221ea02a821e7feeda9cf'
+    />
 )
 
 const three = (
-    <TrackCard/>
+    <TrackCard 
+        albumName= 'BIRDS OF A FEATHER'
+        artistName = 'Billie Eilish'
+        img = 'https://i.scdn.co/image/ab67616d00001e0271d62ea7ea8a5be92d3c1f62'
+    />
 )
 
 const four = (
-    <TrackCard/>
+    <TrackCard 
+        albumName= 'Gata Only'
+        artistName = 'FloyyMenor, Cris Mj'
+        img = 'https://i.scdn.co/image/ab67616d00001e02c4583f3ad76630879a75450a'
+    />
 )
 
 const five = (
-    <TrackCard/>
+    <TrackCard 
+        albumName= 'Lose Control'
+        artistName = 'Teddy Swims'
+        img = 'https://i.scdn.co/image/ab67616d00001e021d856e66d33e22746c21a09c'
+    />
 )
 
 const six = (
-    <TrackCard/>
+    <TrackCard 
+        albumName= 'End of Beginning'
+        artistName = 'Djo'
+        img = 'https://i.scdn.co/image/ab67616d00001e02fddfffec51b4580acae727c1'
+    />
 )
 
 const seven = (
-    <TrackCard/>
+    <TrackCard 
+        albumName= 'Too Sweet'
+        artistName = 'Hozier'
+        img = 'https://i.scdn.co/image/ab67616d00001e02a5aab55aa65e5f6bd19564a2'
+    />
 )
 
 const eight = (
-    <TrackCard/>
+    <TrackCard 
+        albumName= 'One Of The Girls'
+        artistName = 'The Weeknd'
+        img = 'https://i.scdn.co/image/ab67616d00001e02b0dd6a5cd1dec96c4119c262'
+    />
 )
 
 const nine = (
-    <TrackCard/>
+    <TrackCard 
+        albumName='Cruel Summer'
+        artistName = 'Taylor Swift'
+        img = 'https://i.scdn.co/image/ab67616d00001e02e787cffec20aa2a396a61647'
+    />
 )
 
 const ten = (
-    <TrackCard/>
+    <TrackCard 
+        albumName= 'Die With A Smile'
+        artistName = 'Bruno Mars, Lady Gaga'
+        img = 'https://i.scdn.co/image/ab67616d00001e0282ea2e9e1858aa012c57cd45'
+    />
 )
 
 
--- frontend/src/components/TopArtists.js
+++ frontend/src/components/TopArtists.js
@@ -1,45 +1,75 @@
 import React from 'react';
-import ArtistCard from './ArtistCard.js';
+import ArtistCard from './ArtistCard';
 import './TopArtists.css';
 
 const one = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Taylor Swift'
+    img = 'https://i.scdn.co/image/ab67616100005174e672b5f553298dcdccb0e676'
+    />
 )
 
 const two = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'The Weeknd'
+    img = 'https://i.scdn.co/image/ab676161000051749e528993a2820267b97f6aae'
+    />
 )
 
 const three = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Bad Bunny'
+    img = 'https://i.scdn.co/image/ab67616100005174744a4243fb6cc938011a98f4'
+    />
 )
 
 const four = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Drake'
+    img = 'https://i.ebayimg.com/images/g/ovAAAOSwsulmLKPp/s-l1600.webp'
+    />
 )
 
 const five = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Billie Eilish'
+    img = 'https://i.scdn.co/image/ab676161000051744a21b4760d2ecb7b0dcdc8da'
+    />
 )
 
 const six = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Travis Scott'
+    img = 'https://i.scdn.co/image/ab6761610000517419c2790744c792d05570bb71'
+    />
 )
 
 const seven = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Peso Pluma'
+    img = 'https://i.scdn.co/image/ab67616100005174e5283f5b671cf618b82a2696'
+    />
 )
 
 const eight = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Kanye West'
+    img = 'https://i.scdn.co/image/ab676161000051746e835a500e791bf9c27a422a'
+    />
 )
 
 const nine = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Ariana Grande'
+    img = 'https://i.scdn.co/image/ab6761610000517440b5c07ab77b6b1a9075fdc0'
+    />
 )
 
 const ten = (
-    <ArtistCard />
+    <ArtistCard 
+    artistName = 'Feid'
+    img = 'https://photos.bandsintown.com/thumb/19696920.jpeg'
+    />
 )
 
 
